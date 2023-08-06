42c661d3d065244844ea09bcd5000893ba65bd25
Maria José Solano <majosolano99@gmail.com>
2023-08-06 16:24:07 -0700

    Remove struct-patch

--- src/config.rs
+++ src/config.rs
@@ -2,7 +2,6 @@ use anyhow::{anyhow, Context, Result};
 use ratatui::style::Color;
 use serde::Deserialize;
 use std::{fs, io};
-use struct_patch::Patch;
 
 /// `binocular`'s configuration folder name.
 const CONFIG_DIR: &str = "binocular";
@@ -11,12 +10,14 @@ const CONFIG_DIR: &str = "binocular";
 const CONFIG_FILE: &str = "config.json";
 
 #[derive(Deserialize, PartialEq)]
+#[derive(Default, Deserialize)]
+#[serde(default)]
 pub struct ConfigColors {
-    base: Color,
+    base: Option<Color>,
 }
 
-#[derive(PartialEq, Patch)]
-#[patch_derive(Deserialize)]
+#[derive(Default, Deserialize)]
+#[serde(default)]
 pub struct Config {
     colors: ConfigColors,
 }
@@ -30,15 +31,11 @@ impl Config {
         fs::create_dir_all(&config_dir).context("Failed to create configuration directory")?;
 
         // Load the user's configuration file and merge it with the defaults.
-        let mut config = Self::default();
         let config = match fs::read_to_string(config_dir.join(CONFIG_FILE)) {
             Ok(user_config) => {
-                let user_config: ConfigPatch = serde_json::from_str(&user_config)
-                    .context("Failed to parse configuration file")?;
-                config.apply(user_config);
-                config
+                serde_json::from_str(&user_config).context("Failed to parse configuration file")?
             }
-            Err(err) if err.kind() == io::ErrorKind::NotFound => config,
+            Err(err) if err.kind() == io::ErrorKind::NotFound => Self::default(),
             Err(err) => return Err(err).context("Failed to read configuration file"),
         };
 
@@ -46,16 +43,6 @@ impl Config {
     }
 
     pub(crate) fn base_color(&self) -> Color {
-        self.colors.base
-    }
-}
-
-impl Default for Config {
-    fn default() -> Self {
-        Self {
-            colors: ConfigColors {
-                base: Color::LightCyan,
-            },
-        }
+        self.colors.base.unwrap_or(Color::LightCyan)
     }
 }
